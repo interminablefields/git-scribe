2cbe510f814b3272ba6039e3d1407000024625df
janya <budaraju.janya@gmail.com>
2025-04-05 13:57:22 -0400

    fix: adding empty scope option

--- gumtool.sh
+++ gumtool.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 # type dropdown
-TYPE=$(cat config/types.txt | gum choose)
+TYPE=$(cat config/types.txt | gum choose --header "commit type")
 
 # scope input, wrap in parentheses if provided
-SCOPE=$(gum input --placeholder "optional scope- noun describing modified section of codebase")
+SCOPE=$((echo "[ ]"; cat config/scopes.txt) | gum choose --header "optional scope")
+[ "$SCOPE" = "[ ]" ] && SCOPE=""
+
 test -n "$SCOPE" && SCOPE="($SCOPE)"
 
 # collect description and body
-DESC=$(gum input --value "$TYPE$SCOPE: " --placeholder "mandatory description - one line summary of changes")
+DESC=$(gum input \
+		--placeholder "mandatory description - one line summary of changes" \
+		--value "$TYPE$SCOPE: ") 
 BODY=$(gum write --placeholder "optional body - multiline space for elaboration")
 
 # assemble & check w user before committing!
-printf "%b" "$DESC\n\n$BODY" | gum style --border normal --margin "1 2" --padding "1 2" --foreground 212
+printf "%b" "$DESC\n\n$BODY" | gum style --border double --margin "1 2" --padding "1 2" --foreground 212
 gum confirm "commit changes?" && git commit -m "$DESC" -m "$BODY"
